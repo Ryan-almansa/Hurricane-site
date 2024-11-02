865bd92364fc7becd524f7d428b5660becb05d5c
ryan <ryan.almansa@gmail.com>
2024-11-02 13:43:48 +0100

    image refait avec js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,10 @@
-/*!
-* Start Bootstrap - Shop Homepage v5.0.6 (https://startbootstrap.com/template/shop-homepage)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+// Initialisation du compteur de visiteurs
+let visitorCount = 183;
+
+function incrementVisitorCount() {
+    visitorCount += Math.floor(Math.random() * 3); // Incrément aléatoire entre 0 et 2
+    document.getElementById("visitorCount").innerText = visitorCount;
+}
+
+// Mise à jour toutes les 2 secondes
+setInterval(incrementVisitorCount, 2000);
